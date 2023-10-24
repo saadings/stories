3d6ea0e28cfa95a86aa2d9cc59cc48faed77c56c
saadings <saadijutt007@gmail.com>
2023-10-24 21:46:32 +0500

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,9 +2,9 @@ import { StoryType } from "../types/redux/storySlice";
 
 const Card = ({ story }: { story: StoryType }) => {
   return (
-    <div className="h-fit w-fit hover:scale-105 duration-[400ms] transition ease-in-out">
-      <div className="rounded-lg bg-white p-5 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-700 sm:h-[450px] lg:h-[445px]">
-        <a href={story.url}>
+    <a href={story.url} target="_blank" rel="noopener noreferrer">
+      <div className="h-fit w-fit transition duration-[400ms] ease-in-out hover:scale-105">
+        <div className="rounded-lg bg-white p-5 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-700 sm:h-[450px] lg:h-[445px]">
           <img
             src={story.multimedia[1].url}
             alt={story.multimedia[1].caption}
@@ -12,15 +12,15 @@ const Card = ({ story }: { story: StoryType }) => {
             height={story.multimedia[1].height}
             className="rounded-lg"
           />
-        </a>
-        <div className="mt-2 space-y-3">
-          <div className="text-xl font-bold dark:text-gray-300">
-            {story.title}
+          <div className="mt-2 space-y-3">
+            <div className="text-xl font-bold dark:text-gray-300">
+              {story.title}
+            </div>
+            <div className="text-sm dark:text-gray-400">{story.abstract}</div>
           </div>
-          <div className="text-sm dark:text-gray-400">{story.abstract}</div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
